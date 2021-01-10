75a4bcb3b43a51be52931d31104535f2c9017096
kong-fan-xing123 <kfstary@outlook.com>
2021-01-10 15:00:33 +0800

    run to install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #
 echo Extracting color
 cat >color <<\THE-END-OF-DATA
-#!/bin/bash -x
+#!/bin/bash
 for STYLE in 0 1 2 3 4 5 6 7; do
   for FG in 30 31 32 33 34 35 36 37; do
     for BG in 40 41 42 43 44 45 46 47; do
